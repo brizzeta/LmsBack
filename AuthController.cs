1d702bcc7a194fe23d89913e241fdbcd23232637
DiktatorMira <112933359+DiktatorMira@users.noreply.github.com>
2024-06-27 13:24:32 +0300

    Add files via upload
    Добавил валидацию на сервер, а так же исправил контекст

---
+++ AuthController.cs
@@ -0,0 +1,32 @@
+﻿using LmsBack.Model;
+using LmsBack.Models;
+using LmsBack.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LmsBack.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase {
+        private readonly Context db;
+        private readonly ICryptography cryptography;
+        public AuthController(Context context, ICryptography crypt) {
+            db = context;
+            cryptography = crypt;
+        }
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginVM model) {
+            var account = db.Accounts.SingleOrDefault(a => a.Login == model.Login);
+            if (account == null) return Unauthorized("Неверный логин или пароль.");
+            string hashedPassword = cryptography.HashPassword(model.Password!, account.Salt!);
+            if (hashedPassword != account.Password) return Unauthorized("Неверный логин или пароль.");
+            var admin = db.Admins.SingleOrDefault(a => a.Id == account.Id);
+            if (admin == null) return Unauthorized("Админ не найден.");
+
+            return Ok(new {
+                admin.Name,
+                admin.Surname,
+                admin.Email
+            });
+        }
+    }
+}
---
+++ Context.cs
@@ -0,0 +1,70 @@
+﻿using LmsBack.Model;
+using LmsBack.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace LmsBack {
+    public class Context : DbContext {
+        #region Дбсеты
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Model.DayOfWeek> DayOfWeeks { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Manager> Managers { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Parent> Parents { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Attendence> Attendences { get; set; }
+        public DbSet<HomeworkMark> HomeworkMarks { get; set; }
+        public DbSet<HomeworkStudent> HomeworkStudents { get; set; }
+        public DbSet<HomeworkTeacher> HomeworkTeachers { get; set; }
+        public DbSet<Lesson> Lessons { get; set; }
+        public DbSet<LessonMark> LessonMarks { get; set; }
+        public DbSet<Schedule> Schedules { get; set; }
+        public DbSet<TimeOfLesson> TimeOfLessons { get; set; }
+        #endregion
+        ICryptography cryptography;
+        public Context(DbContextOptions<Context> options, ICryptography crypt) : base(options) {
+            if (Database.EnsureCreated()) {
+                cryptography = crypt;
+                Roles!.AddRange(new List<Role>() {
+                    new Role { Name="Director" },
+                    new Role { Name="Admin" }
+                });
+                DayOfWeeks!.AddRange(new List<Model.DayOfWeek>() {
+                    new Model.DayOfWeek { Name = "Monday" },
+                    new Model.DayOfWeek { Name = "Tuesday" },
+                    new Model.DayOfWeek { Name = "Wednesday" },
+                    new Model.DayOfWeek { Name = "Thursday" },
+                    new Model.DayOfWeek { Name = "Friday" },
+                    new Model.DayOfWeek { Name = "Saturday" },
+                    new Model.DayOfWeek { Name = "Sunday" }
+                });
+                string salt = cryptography.GenerateSalt();
+                Accounts!.AddRange(new List<Account>() {
+                    new Account {
+                        Login = "Diktator",
+                        Password = cryptography.HashPassword("WeRt2345", salt),
+                        Salt = salt
+                    }
+                });
+                SaveChanges();
+                Admins!.AddRange(new List<Admin>() {
+                    new Admin {
+                        Name = "Данило",
+                        Surname = "Червоний",
+                        Patronymic = "Юрійович",
+                        Phone = "+380502953439",
+                        Email = "dychervony@gmail.com",
+                        BirthDate = new DateTime(2004, 8, 13),
+                        Account = Accounts.FirstOrDefault(a => a.Login == "Diktator"),
+                        Role = Roles.FirstOrDefault(r => r.Name == "Director")
+                    }
+                });
+                SaveChanges();
+            }
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseLazyLoadingProxies();
+    }
+}
